b655e504408f026047057bfe56c502b72064e1a3
kabouter <kabouter@users.noreply.github.com>
2015-06-13 01:55:16 +0200

    Update and rename dutch to nl_lang.php

---
+++ application/language/dutch/nl_lang.php
@@ -0,0 +1,428 @@
+<?php
+/**
+ * Language definitions  
+ *  
+ * @package		ci_fullcalendar
+ * @category    Language
+ * @author		kabouter
+ * @link		/language/dutch/nl_lang
+ */
+ 
+//Common words
+//
+$lang['home'] = 'Home';
+$lang['calendar'] = 'Kalendar';
+$lang['events'] = 'Evenement';
+$lang['dashboard'] = 'Dashboard';
+$lang['tables'] = 'Events Tables';
+$lang['page'] = 'Pagina';
+$lang['pages'] = 'Pages';
+$lang['share'] = 'Delen'; 
+$lang['users'] = 'Gebruikers'; 
+$lang['user'] = 'Gebruiker'; 
+$lang['admin'] = 'Admin'; 
+$lang['lock'] = 'Lock'; 
+$lang['unlock'] = 'Unlock'; 
+$lang['maps'] = 'Maps'; 
+$lang['time'] = 'Tijd'; 
+$lang['by'] = 'door'; 
+$lang['hi'] = 'Hi'; 
+$lang['yes'] = 'ja'; 
+$lang['no'] = 'nee'; 
+$lang['true'] = 'Juist'; 
+$lang['false'] = 'Fout'; 
+$lang['all'] = 'Alles';  
+$lang['actions'] = 'Acties';  
+$lang['username'] = 'Gebruikersnaam'; 
+$lang['password'] = 'Wachtwoord'; 
+$lang['business'] = 'Bedrijf'; 
+$lang['geocodes'] = 'Geocodes'; 
+$lang['address'] = 'Adres'; 
+$lang['timezone'] = 'Tijdszone'; 
+$lang['pubdate'] = 'Publicatiedatum'; 
+$lang['detail'] = 'Detail'; 
+$lang['name'] = 'Naam'; 
+$lang['description'] = 'Beschrijving'; 
+$lang['add'] = 'Toevoegen'; 
+$lang['edit'] = 'Aanpassen'; 
+$lang['delete'] = 'Verwijderen'; 
+$lang['records'] = 'records'; 
+$lang['source'] = 'Bron'; 
+$lang['attach'] = 'Toevoegen'; 
+$lang['ok'] = 'Ok'; 
+$lang['url'] = 'URL'; 
+$lang['save'] = 'Opslaan'; 
+$lang['cancel'] = 'Annuleren'; 
+$lang['lat'] = 'Lat'; 
+$lang['lng'] = 'Lng'; 
+$lang['current_v'] = 'CIFullCalendar+ door Sir.Dre © 2015 v'; 
+//Admin
+//
+$lang['admin_dashboard'] = 'Administrate';
+$lang['admin_page_title'] = 'Kalendar';
+$lang['admin_form_contact'] = 'Contactinformatie';
+$lang['admin_info_title'] = 'Basis Informatie';
+$lang['admin_configuration'] = 'Configureren';
+//Pages
+//
+$lang['page_header'] = 'Pagina\'s';
+$lang['page_title'] = 'Paginatitel';
+$lang['page_content'] = 'Page Contents';
+$lang['page_meta_keywords'] = 'Page Contents';
+$lang['page_meta_description'] = 'Meta beschrijving'; 
+$lang['page_username'] = 'Gebruikersnaam';
+$lang['page_table_edit'] = 'Aanpassen';
+//Edit
+$lang['pages_meta_keywords'] = 'Meta trefwoorden';
+$lang['pages_meta_description'] = 'Meta beschrijvingen';
+$lang['pages_access'] = 'Toegang'; 
+$lang['pages_access_visitors'] = 'Publiek'; 
+$lang['pages_access_members'] = 'Leden'; 
+$lang['pages_access_admins'] = 'Administraters'; 
+//Tables
+//
+$lang['admin_level_2'] = 'Admin Rechten';
+$lang['admin_level_1'] = 'Gebruikers Rechten';
+$lang['admin_status_1'] = 'Locked';
+$lang['admin_status_0'] = 'Unlocked';
+//user
+$lang['admin_modal_edit_user'] = 'Gebruikersinformatie aanpassen';
+$lang['admin_modal_delete_user'] = 'Verwijder gebruikersinformatie';
+$lang['admin_modal_add_user'] = 'Add user Information';
+//calendar
+$lang['admin_modal_delete_calendar'] = 'Delete Calendar Information';
+//Maps
+$lang['admin_modal_maps_calendar'] = 'Delete Maps Information';
+$lang['admin_modal_member_since'] = 'Lid sinds';
+$lang['admin_modal_member_last_log'] = 'Laatste aanmelding';
+$lang['admin_modal_ip'] = 'IP-adres';
+//Columns
+$lang['admin_table_username'] = 'User Name';
+$lang['admin_table_fname'] = 'Voornaam';
+$lang['admin_table_lname'] = 'Familienaam';
+$lang['admin_table_address'] = 'Adres';
+$lang['admin_table_phone'] = 'Phone';
+$lang['admin_table_email'] = 'Email';
+$lang['admin_table_password'] = 'Password';
+$lang['admin_table_level'] = 'Rechten';
+$lang['admin_table_status'] = 'Status';
+$lang['admin_table_edit'] = 'Aanpassen';
+$lang['admin_table_event_title'] = 'Titel';
+$lang['admin_table_event_description'] = 'Description';
+$lang['admin_table_event_start'] = 'Start Date';
+$lang['admin_table_event_end'] = 'End Date';
+$lang['admin_table_event_allday'] = 'Volledige dag';
+$lang['admin_table_event_type'] = 'Publiek/privé';
+$lang['admin_table_event_noshow'] = 'privé';
+$lang['admin_table_event_show'] = 'Publiek';
+$lang['admin_table_markers_various_date'] = 'Verschillende datums';
+$lang['admin_table_markers_info'] = 'Info';
+$lang['admin_table_markers_name'] = 'Titel';
+$lang['admin_table_markers_address'] = 'Adres';
+$lang['admin_table_markers_category'] = 'Categorie';
+$lang['admin_table_markers_description'] = 'Beschrijving';
+$lang['admin_table_markers_url'] = 'URL';
+ 
+//Profile
+//
+$lang['profile_dashboard'] = 'My Home';
+$lang['profile_page_title'] = 'Kalendar';
+$lang['profile_form_contact'] = 'Mijn contactinformatie';
+$lang['profile_info_title'] = 'Basis Informatie';
+$lang['profile_configuration'] = 'Configureren';
+  
+//Profile page
+$lang['profile_page'] = 'Profiel';
+$lang['profile_welcome'] = 'Welkom ';
+ 
+//Login
+$lang['profile_login'] = 'Aanmelden';
+$lang['profile_signin'] = 'sign-in';
+$lang['profile_noaccount'] = 'Heeft u nog geen account?';
+$lang['profile_login_username'] = 'Gebruikersnaam';
+$lang['profile_login_password'] = 'Password';
+$lang['profile_logged_in'] = 'Aanmelden als';
+$lang['profile_log_out'] = 'Afmelden';
+$lang['profile_login_fail'] = 'Sorry, user login info do not exist';
+$lang['profile_login_auth_fail'] = 'Sorry, authentication failed'; 
+ 
+$lang['profile_signup_date'] = 'Lid sinds';
+$lang['profile_signin_date'] = 'Sign-in Since';
+$lang['profile_signin_error'] = 'Sorry Password is incorrect';
+$lang['profile_login_noemail'] = 'Sorry, email do not exist';
+$lang['profile_email_exists'] = 'Sorry, email already exist';
+$lang['profile_captcha'] = 'Sorry, the captcha code incorrect. Please try again';
+//Logout
+$lang['profile_logout'] = 'Afmelden';
+//Forgot Password
+$lang['profile_forgot_password'] = 'Wachtwoord vergeten';
+//Register
+$lang['register'] = 'Registreren';
+$lang['profile_signup'] = 'sign-up';
+$lang['profile_register'] = 'New Member Registration';
+$lang['profile_register_uname'] = 'User name';
+$lang['profile_register_password'] = 'Password';
+$lang['profile_register_email'] = 'Email';  
+$lang['profile_register_success'] = 'Thank you for registering, you are able to log-in';
+$lang['profile_register_button'] = 'Register';
+$lang['profile_register_captcha'] = 'Captcha';
+$lang['profile_register_fail'] = 'Your changes were NOT submitted, see the messages below';
+$lang['register_email_message_subject'] = 'Registration Message';
+$lang['register_email_message1'] = 'Welcome to ';
+$lang['register_email_message2'] = 'The calendar, allows you to retrieve all events from supported database. Also, there are options to add, update, delete events and have them public or private.';
+$lang['register_email_message3'] = 'Sign-in Information:';
+//Change Password
+$lang['profile_change_password'] = 'Change user password';
+$lang['profile_change_warning'] = 'You are about to change';
+$lang['profile_change_password_text'] = 'Old Password';
+$lang['profile_change_password_text2'] = 'New Password';
+//Reset Password
+$lang['reset_password'] = 'Reset Password';
+$lang['recover_password'] = 'Change Password';
+//Forgot Password
+$lang['forgot_login_email'] = 'Email';
+$lang['forgot_login_email_submit'] = 'Recover Password';
+$lang['forgot_login_email_send'] = 'Password Recovery Information sent';
+$lang['forgot_login_subject'] = 'Password Recovered';
+$lang['forgot_login_message'] = 'A password confirmation is in your email';
+$lang['forgot_login_email_message1'] = 'Password Change/Recovery';
+$lang['forgot_login_email_message2'] = 'You have requested a password change. If I haven\'t requested this change please ignore email or contact your administrator.';
+$lang['forgot_login_email_message3'] = 'Below is the link to click to confirm change. Another email will follow with a temporary password.';
+//Edit
+$lang['profile_edit_basic_info'] = 'Edit Basic Info'; 
+$lang['profile_edit_info'] = 'Edit Profile';
+$lang['profile_edit_goback'] = 'Go back to Profile';
+$lang['profile_edit_success'] = 'Your changes have been saved';
+$lang['profile_edit_logo_title'] = 'Upload new profile pic';  
+$lang['profile_edit_logo_upload'] = 'Upload your profile pic';  
+$lang['profile_edit_logo_preview'] = 'Your current profile pic';  
+$lang['profile_edit_uname'] = 'User Name';
+$lang['profile_edit_fname'] = 'First Name';
+$lang['profile_edit_lname'] = 'Last Name';
+$lang['profile_edit_age'] = 'Age';
+$lang['profile_edit_address'] = 'Address';
+$lang['profile_edit_phone'] = 'Phone';
+$lang['profile_edit_email'] = 'Email';
+$lang['profile_edit_password'] = 'Password'; 
+$lang['profile_form_upload_error'] = 'Sorry, unable to upload profile picture';
+$lang['profile_form_fail_email'] = 'Email already exist for this user';
+$lang['profile_form_fail'] = 'Sorry, your profile information did not save';
+$lang['profile_form_success'] = 'Success, your profile information saved';
+$lang['profile_form_submit_button'] = 'Update Profile';
+ 
+//Delete
+$lang['profile_delete_profile'] = 'Profiel';
+$lang['profile_delete_warning'] = 'You are about to delete';
+$lang['profile_delete_uname'] = 'Gebruikersnaam - ';
+$lang['profile_delete_confirm'] = 'Verwijdering bevestigen';
+$lang['profile_delete_cancel'] = 'Verwijder profiel';
+ 
+//Button sub-menu
+//
+$lang['submenu_select_categories'] = 'Select Categories'; 
+$lang['submenu_select_sources'] = 'Select Sources'; 
+$lang['submenu_dropdown_all_locations'] = 'All Locations'; 
+$lang['submenu_dropdown_all_categories'] = 'All Categories'; 
+$lang['submenu_dropdown_all_sources'] = 'All Sources'; 
+  
+//Calendar Modal
+//
+$lang['calendar_title'] = 'Events';
+$lang['calendar_title_public'] = 'Public Events';
+$lang['calendar_loading_title'] = ' loading...	Please Wait.  ';
+$lang['calendar_export'] = 'Export';
+$lang['calendar_import'] = 'Import';
+$lang['calendar_modal_create_title'] = 'Create Event';
+$lang['calendar_modal_update_title'] = 'Update Event'; 
+$lang['calendar_modal_tabtitle'] = 'Info';
+$lang['calendar_modal_tabtitle2'] = 'Category';
+$lang['calendar_modal_tabtitle3'] = 'Location';
+$lang['calendar_modal_tabtitle4'] = 'Attach';
+$lang['calendar_modal_eventname'] = 'Title';
+$lang['calendar_modal_description'] = 'Description';
+$lang['calendar_modal_eventbegin'] = 'Begin Event';
+$lang['calendar_modal_eventend'] = 'End Event';
+$lang['calendar_modal_eventurl'] = 'URL Link';
+$lang['calendar_modal_eventallday'] = 'All Day';
+$lang['calendar_modal_eventshare'] = 'Share Event';
+$lang['calendar_modal_eventprivate'] = 'Private Event';
+$lang['calendar_modal_eventpublic'] = 'Public Event'; 
+$lang['calendar_modal_eventrecurring'] = 'Recurring Events';
+$lang['calendar_modal_eventendrecurring'] = 'End';
+$lang['calendar_modal_eventnonerecurring'] = 'Non-recurring';
+$lang['calendar_modal_eventdaily'] = 'Daily';
+$lang['calendar_modal_eventweekly'] = 'Weekly';
+$lang['calendar_modal_event2weeks'] = 'Every 2 Weeks';
+$lang['calendar_modal_eventmonthly'] = 'Monthly';
+$lang['calendar_modal_eventyearly'] = 'Yearly'; 
+$lang['calendar_modal_eventrendering'] = 'Placement';  
+$lang['calendar_modal_eventfgrender'] = 'Foreground'; 
+$lang['calendar_modal_eventbgrender'] = 'Background';  
+$lang['calendar_modal_eventcategory'] = 'Category'; 
+$lang['calendar_modal_colorbackground'] = 'Background Color'; 
+$lang['calendar_modal_colortext'] = 'Text Color'; 
+$lang['calendar_modal_colorborder'] = 'Border Color';
+$lang['calendar_modal_attachment'] = 'Attach File'; 
+$lang['calendar_modal_upload_eventsource'] = 'Upload Events Source'; 
+$lang['calendar_modal_upload_event_message'] = 'Select ical file(ics) from your computer'; 
+$lang['calendar_modal_upload_save'] = 'Upload ICal'; 
+ 
+//Notify
+//
+$lang['notify_email_message'] = 'Nieuw publiek evenement beschikbaar'; 
+ 
+//Gmaps
+//
+$lang['locations_all_heading'] = 'Alle locaties'; 
+//Count
+$lang['locations_all_confirm_locations'] = 'All Confirm locations'; 
+  
+  
+//Categories
+//
+$lang['categories_all_heading'] = 'All Category';
+//Add
+$lang['categories_add_new'] = 'Add New Category';
+$lang['categories_input_name'] = 'Category Name';
+$lang['categories_input_description'] = 'Category Description';
+//Edit
+$lang['categories_edit_source'] = 'Edit this Category';
+//Delete
+$lang['categories_delete_category'] = 'Delete this Category';
+$lang['categories_delete_warning'] = 'You are about to delete';
+//Message
+$lang['categories_message_title'] = 'No Categories';
+$lang['categories_message_warning'] = 'No Categories available';
+ 
+//Sources
+//
+$lang['sources_all_heading'] = 'All Sources';
+//Add
+$lang['sources_add_new'] = 'Add New Source';
+$lang['sources_input_name'] = 'Source Name';
+$lang['sources_input_url'] = 'Source URL';
+//Edit
+$lang['sources_edit_source'] = 'Edit this Source';
+//Delete
+$lang['sources_del_source'] = 'Delete this Source';
+$lang['sources_delete_warning'] = 'You are about to delete';
+//Message
+$lang['sources_message_title'] = 'No Sources';
+$lang['sources_message_warning'] = 'No Sources available';
+ 
+ 
+//Settings
+//
+$lang['settings_name'] = 'Settings';
+$lang['settings_basic_name'] = 'Basic Settings';
+$lang['settings_cal_name'] = 'Calendar Settings';
+$lang['settings_file_name'] = 'ICal Settings';
+$lang['settings_pic_name'] = 'Pic Settings';
+$lang['settings_attach_name'] = 'Attach Settings';
+$lang['settings_theme_name'] = 'Theme Settings';
+//Edit
+$lang['settings_form_site_name'] = 'Site Name';
+$lang['settings_form_site_email'] = 'Site Email';
+$lang['settings_form_meta_keywords'] = 'Site meta keywords';
+$lang['settings_form_meta_description'] = 'Site meta description';
+$lang['settings_form_timezone'] = 'Timezone';
+$lang['settings_form_captcha'] = 'Captcha';
+$lang['settings_form_debug'] = 'Debug';
+$lang['settings_form_submit_button'] = 'Submit';
+//fullcalendar
+$lang['cal_defaultview'] = 'Default View';
+$lang['cal_defaultview_month'] = 'Month';
+$lang['cal_defaultview_basicweek'] = 'BasicWeek';
+$lang['cal_defaultview_basicday'] = 'BasicDay';
+$lang['cal_defaultview_agendaweek'] = 'AgendaWeek';
+$lang['cal_defaultview_agendaday'] = 'AgendaDay';
+$lang['cal_defaultview_agendalist'] = 'AgendaList';
+ 
+$lang['cal_header_left'] = 'Header Button/Title Left';
+$lang['cal_header_center'] = 'Header Button/Title Center';
+$lang['cal_header_right'] = 'Header Button/Title Right';
+$lang['cal_language'] = 'Language';
+$lang['cal_hiddendays'] = 'HideDays (0=Sun,1=Mon,2=Tue,3=Wed,4=Thu,5=Fri,6=Sat)';
+$lang['cal_editable'] = 'Editable (For members calendar only)';
+$lang['cal_weeknumbers'] = 'Week Numbers';
+$lang['cal_eventlimit'] = 'Event Limit';
+$lang['cal_alldayslot'] = 'AllDay Slot';
+$lang['cal_isrtl'] = 'View from Right To Left';
+ 
+//Theme 
+$lang['theme_name'] = 'Theme';
+//Edit
+$lang['profile_picture_title'] = 'Profile Picture Requirement';
+$lang['file_title'] = 'ICS File Requirement';
+$lang['attach_title'] = 'Attachment File Requirement';
+$lang['profile_max_upload_width'] = 'Width';
+$lang['profile_max_upload_height'] = 'Height';
+$lang['profile_max_upload_filesize'] = 'Filesize  (KB)';
+$lang['profile_allowed_extensions'] = 'Allowed Extension'; 
+$lang['sync_path_location'] = 'ICS File Location';
+$lang['sync_allowed_extension'] = 'Allowed Extension';
+$lang['sync_max_size'] = 'Filesize (KB)'; 
+$lang['attach_allowed_extension'] = 'Allowed Extension';
+$lang['attach_max_size'] = 'Filesize (KB)'; 
+//Language
+//
+$lang['theme_language'] = 'Language';
+//lang choices
+$lang['lang_english'] = 'English';
+$lang['lang_arabic_ma'] = 'Arabic MA'; 
+$lang['lang_arabic_sa'] = 'Arabic SA';  
+$lang['lang_arabic_tn'] = 'Arabic TN';  
+$lang['lang_arabic'] = 'Arabic';    
+$lang['lang_bulgarian'] = 'Bulgarian';  
+$lang['lang_catalan'] = 'Catalan';  
+$lang['lang_czech'] = 'Czech';  
+$lang['lang_danish'] = 'Danish';  
+$lang['lang_german_at'] = 'German AT';  
+$lang['lang_german'] = 'German';  
+$lang['lang_greek'] = 'Greek';   
+$lang['lang_english_au'] = 'English AU';  
+$lang['lang_english_ca'] = 'English CA';  
+$lang['lang_english_gb'] = 'English GB';  
+$lang['lang_spanish'] = 'Spanish';  
+$lang['lang_farsi'] = 'Farsi';   
+$lang['lang_finnish'] = 'Finnish';    
+$lang['lang_french_ca'] = 'French CA'; 
+$lang['lang_french'] = 'French';  
+$lang['lang_hebrew'] = 'Hebrew'; 
+$lang['lang_hindi'] = 'Hindi';  
+$lang['lang_croatian'] = 'Croatian'; 
+$lang['lang_hungarian'] = 'Hungarian';  
+$lang['lang_indonesian'] = 'Indonesian'; 
+$lang['lang_icelandic'] = 'Icelandic'; 
+$lang['lang_italian'] = 'Italian';  
+$lang['lang_japanese'] = 'Japanese';
+$lang['lang_korean'] = 'Korean';  
+$lang['lang_lithuanian'] = 'Lithuanian';  
+$lang['lang_latvian'] = 'Latvian';  
+$lang['lang_norwegian'] = 'Norwegian';  
+$lang['lang_dutch'] = 'Dutch';  
+$lang['lang_polish'] = 'Polish';  
+$lang['lang_portuguese_br'] = 'Portuguese BR';  
+$lang['lang_portuguese'] = 'Portuguese';  
+$lang['lang_romanian'] = 'Romanian';   
+$lang['lang_russian'] = 'Russian';  
+$lang['lang_slovak'] = 'Slovak';  
+$lang['lang_slovenian'] = 'Slovenian';  
+$lang['lang_serbian_cyrl'] = 'Serbian CYRL';  
+$lang['lang_serbian'] = 'Serbian';  
+$lang['lang_swedish'] = 'Swedish';   
+$lang['lang_thai'] = 'Thai';  
+$lang['lang_turkish'] = 'Turkish';  
+$lang['lang_ukrainian'] = 'Ukrainian';  
+$lang['lang_vietnamese'] = 'Vietnamese';  
+$lang['lang_chinese'] = 'Chinese';  
+$lang['lang_chinese_tw'] = 'Chinese TW';
+ 
+//Error
+// 
+$lang['error_not_found'] = 'Het spijt ons.... we konden de pagina niet vinden';
+$lang['error_page_title'] = 'Error';
+$lang['error_not_found_page_title'] = 'Niet gevonden';
+$lang['error_sending_email'] = 'There was a problem sending an email... contact the site administrator';
+$lang['error_creating_password'] = 'There was a problem creating a new password... contact the site administrator';
+ 
