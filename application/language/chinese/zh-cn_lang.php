2ae286f4886618c65fd9bf3ee1a8a479c287bd54
Andre Gardiner <andre@sirdre.com>
2016-03-14 12:50:31 -0600

    Language updates
    CIFullCalendar language updates for v2/v3

---
+++ application/language/chinese/zh-cn_lang.php
@@ -0,0 +1,563 @@
+<?php
+/**
+ * Language definitions  
+ *  
+ * @package		ci_fullcalendar
+ * @category    Language
+ * @author		sirdre
+ * @link		/language/chinese/zh-cn_lang
+ */
+ 
+//Common words
+//
+$lang['home'] = '家';
+$lang['calendar'] = '日历';
+$lang['events'] = '事件';
+$lang['dashboard'] = '仪表板';
+$lang['tables'] = '表';
+$lang['page'] = '页';
+$lang['pages'] = '页数';
+$lang['share'] = '分享';
+$lang['user'] = '用户';
+$lang['users'] = '用户';
+$lang['member'] = '会员';
+$lang['members'] = '会员';
+$lang['admin'] = '管理员';
+$lang['admins'] = '管理员';
+$lang['group'] = '组';
+$lang['groups'] = '网上论坛';
+$lang['lock'] = '锁';
+$lang['unlock'] = '开锁';
+$lang['maps'] = '地图';
+$lang['time'] = '时间';
+$lang['by'] = '通过';
+$lang['hi'] = '嗨';
+$lang['yes'] = '是';
+$lang['no'] = '没有';
+$lang['true'] = '真正';
+$lang['false'] = '假';
+$lang['all'] = '所有';
+$lang['actions'] = '操作';
+$lang['username'] = '用户名';
+$lang['password'] = '密码';
+$lang['business'] = '商业';
+$lang['geocodes'] = '地理编码';
+$lang['address'] = '地址';
+$lang['timezone'] = '时区';
+$lang['pubdate'] = '发布日期';
+$lang['detail'] = '详情';
+$lang['name'] = '名称';
+$lang['description'] = '描述';
+$lang['add'] = '加';
+$lang['edit'] = '编辑';
+$lang['delete'] = '删除';
+$lang['records'] = '记录';
+$lang['source'] = '资源';
+$lang['attach'] = '连接';
+$lang['ok'] = '好';
+$lang['url'] = 'URL';
+$lang['save'] = '保存';
+$lang['cancel'] = '取消';
+$lang['monday'] = '星期一';
+$lang['tuesday'] = '星期二';
+$lang['wednesday'] = '星期三';
+$lang['thursday'] = '星期四';
+$lang['friday'] = '星期五';
+$lang['saturday'] = '星期六';
+$lang['sunday'] = '星期日';
+$lang['lat'] = '纬度';
+$lang['lng'] = '经度';
+$lang['approve'] = '批准';
+$lang['decline'] = '下降';
+$lang['queue'] = '队列';
+$lang['queues'] = '队列';
+$lang['sessions'] = '会议';
+$lang['register'] = '寄存器';
+$lang['notify'] = '通知';
+$lang['identity'] = '身份';
+$lang['search'] = '搜索';
+$lang['options'] = '选项';
+$lang['alert'] = '警报';
+$lang['current_v'] = 'CIFullCalendar Sir.Dre © 2016 年v';
+	
+//Admin	//管理员
+//	//
+$lang['admin_dashboard'] = '管理';
+$lang['admin_page_title'] = '日历';
+$lang['admin_form_contact'] = '我的联系信息';
+$lang['admin_info_title'] = '基本信息';
+$lang['admin_configuration'] = '配置';
+	
+//Templates	//模板
+//  	//
+$lang['templates_title'] = '电子邮件模板';
+$lang['templates_name'] = '标题';
+$lang['templates_content'] = '身体';
+//nav	//导航
+$lang['templates_nav_notify'] = '通知';
+$lang['templates_nav_register'] = '寄存器';
+$lang['templates_nav_forgot_password'] = '确认密码';
+$lang['templates_nav_reset_password'] = '重设密码';
+$lang['templates_nav_change_email'] = '电子邮件确认';
+$lang['templates_nav_reset_email'] = '电子邮件复位';
+	
+//Pages	//网页
+//	//
+$lang['page_header'] = '页数';
+$lang['page_title'] = '页面标题';
+$lang['page_content'] = '页面内容';
+$lang['page_meta_keywords'] = '网页关键字';
+$lang['page_meta_description'] = '元说明';
+$lang['page_username'] = '用户名';
+$lang['page_table_edit'] = '编辑';
+//Edit	//编辑
+$lang['pages_meta_keywords'] = 'Meta关键字';
+$lang['pages_meta_description'] = 'Meta描述';
+$lang['pages_access'] = '视图访问';
+$lang['pages_access_visitors'] = '上市';
+$lang['pages_access_members'] = '会员';
+$lang['pages_access_admins'] = '管理员';
+//Delete	//删除
+$lang['pages_delete_page'] = '删除此页';
+$lang['pages_delete_warning'] = '你要删除';
+//Message	//信息
+$lang['pages_message_title'] = '无页';
+$lang['pages_message_warning'] = '没有可用的页面';
+$lang['sessions_message_title'] = '不会话';
+$lang['sessions_message_warning'] = '没有可用的会话';
+	
+//Tables	//表
+// 	//
+$lang['admin_status_1'] = '活性';
+$lang['admin_status_0'] = '无效';
+//modals	//情态动词
+$lang['admin_modal_edit_user'] = '编辑用户信息';
+$lang['admin_modal_delete_user'] = '删除用户信息';
+$lang['admin_modal_add_user'] = '添加用户信息';
+$lang['admin_modal_delete_calendar'] = '删除日历信息';
+$lang['admin_modal_maps_calendar'] = '删除信息地图';
+$lang['admin_modal_member_since'] = '会员参加';
+$lang['admin_modal_member_last_log'] = '上次登录';
+$lang['admin_modal_ip'] = 'IP地址';
+	
+//Toolbar	//工具栏
+$lang['admin_remove_session'] = '清除会话';
+	
+//Columns	//列
+$lang['admin_table_username'] = '用户名';
+$lang['admin_table_fname'] = '名字';
+$lang['admin_table_lname'] = '姓';
+$lang['admin_table_address'] = '地址';
+$lang['admin_table_company'] = '公司';
+$lang['admin_table_phone'] = '电话';
+$lang['admin_table_email'] = '电子邮件';
+$lang['admin_table_password'] = '密码';
+$lang['admin_table_level'] = '特权';
+$lang['admin_table_status'] = '状态';
+$lang['admin_table_edit'] = '编辑';
+$lang['admin_table_event_title'] = '标题';
+$lang['admin_table_event_description'] = '描述';
+$lang['admin_table_event_start'] = '开始日期';
+$lang['admin_table_event_end'] = '结束日期';
+$lang['admin_table_event_allday'] = '整天';
+$lang['admin_table_event_type'] = '公共/私人';
+$lang['admin_table_event_noshow'] = '私人的';
+$lang['admin_table_event_show'] = '上市';
+$lang['admin_table_markers_various_date'] = '各种日期';
+$lang['admin_table_markers_info'] = '信息';
+$lang['admin_table_markers_name'] = '标题';
+$lang['admin_table_markers_address'] = '地址';
+$lang['admin_table_markers_category'] = '类别';
+$lang['admin_table_markers_description'] = '描述';
+$lang['admin_table_markers_url'] = 'URL';
+$lang['admin_table_timestamp'] = '会话时间';
+$lang['admin_table_ipaddress'] = 'IP地址';
+$lang['admin_table_uagent'] = '用户代理';
+	
+//nav	//导航
+//	//
+$lang['admin_nav_events'] = '活动列表';
+$lang['admin_nav_queue'] = '活动队列';
+$lang['admin_nav_users'] = '会员';
+$lang['admin_nav_sessions'] = '会议';
+$lang['admin_nav_group'] = '组';
+	
+//Profile	//简介
+//	//
+$lang['profile_dashboard'] = '我的家';
+$lang['profile_page_title'] = '日历';
+$lang['profile_form_contact'] = '我的联系信息';
+$lang['profile_info_title'] = '基本信息';
+$lang['profile_configuration'] = '配置';
+  	
+//Profile page	//个人资料页
+$lang['profile_page'] = '个人资料';
+$lang['profile_welcome'] = '欢迎';
+//Login	//登录
+$lang['profile_login'] = '登录';
+$lang['profile_signin'] = '签到';
+$lang['profile_noaccount'] = '不要 还没有账户';
+$lang['profile_login_username'] = '用户名/电子邮件';
+$lang['profile_login_password'] = '密码';
+$lang['profile_remember_label'] = '记住我';
+$lang['profile_logged_in'] = '登录作为';
+$lang['profile_login_fail'] = '对不起，用户登录信息不存在';
+$lang['profile_login_auth_fail'] = '对不起，验证失败';
+$lang['profile_signup_date'] = '会员参加';
+$lang['profile_signin_date'] = '签到由于';
+$lang['profile_signin_error'] = '对不起，密码不正确';
+$lang['profile_login_noemail'] = '对不起，电子邮件不存在';
+$lang['profile_email_exists'] = '很抱歉，电子邮件已经存在';
+$lang['profile_captcha'] = '对不起，图形验证码不正确。请再试一次';
+//Logout	//登出
+$lang['profile_logout'] = '登出';
+$lang['profile_log_out'] = '登出';
+//Forgot Password	//忘了密码
+$lang['profile_forgot_password'] = '忘了密码';
+//Register	//寄存器
+$lang['profile_signup'] = '注册';
+$lang['profile_register'] = '新会员注册';
+$lang['profile_register_uname'] = '用户名';
+$lang['profile_register_password'] = '密码';
+$lang['profile_register_password_confirm'] = '确认密码';
+$lang['profile_register_email'] = '电子邮件';
+$lang['profile_register_phone'] = '电话';
+$lang['profile_register_success'] = '谢谢你注册，你可以登录';
+$lang['profile_register_button'] = '寄存器';
+$lang['profile_register_captcha'] = '验证码';
+$lang['profile_register_fail'] = '您的更改未提交的，请参见下面的消息';
+$lang['profile_registeration_fail'] = '对不起，会员登录已经存在';
+//Message	//信息
+$lang['register_email_message_subject'] = '注册信息';
+$lang['register_email_message1'] = '欢迎来到 ';
+$lang['register_email_message2'] = '日历为组织，控制和分享活动的力量。轻松登录并通过简单地用奇妙的功​​能添加或更新通过拖动，缩放，点击和感人事件。此外，支持大多数移动设备';
+$lang['register_email_message3'] = '登录信息：';
+//Change Password	//更改密码
+$lang['profile_change_password'] = '更改用户密码';
+$lang['profile_change_warning'] = '你是要改变';
+$lang['profile_change_old_password'] = '旧密码';
+$lang['profile_change_new_password'] = '新密码';
+$lang['profile_change_new_password_confirm'] = '新密码确认';
+//Reset Password	//重设密码
+$lang['reset_password'] = '重设密码';
+$lang['recover_password'] = '更改密码';
+//Forgot Password	//忘了密码
+$lang['forgot_login_email'] = '电子邮件';
+$lang['forgot_login_email_submit'] = '恢复密码';
+$lang['forgot_login_email_send'] = '密码恢复信息发送';
+$lang['forgot_login_subject'] = '密码恢复';
+$lang['forgot_login_message'] = '密码确认是在您的电子邮件';
+//Message	//信息
+$lang['forgot_login_email_message1'] = '密码更改/恢复';
+$lang['forgot_login_email_message2'] = '您已要求更改密码。如果我的天堂\'吨要求这个变化，请忽略电子邮件或与管理员联系。';
+$lang['forgot_login_email_message3'] = '下面是链接到点击确认更改。另一个电子邮件将遵循一个临时密码。';
+//New Email 	//新邮件
+$lang['email_reset_subject'] = '新邮件';
+$lang['email_reset_message'] = '您已经成功地改变你的电子邮件地址';
+$lang['email_reset_message_confirmation'] = '您已成功发送电子邮件确认您的电子邮件地址';
+	
+//Edit	//编辑
+$lang['profile_edit_basic_info'] = '编辑基本信息';
+$lang['profile_edit_info'] = '编辑个人资料';
+$lang['profile_edit_goback'] = '回到个人资料';
+$lang['profile_edit_success'] = '你的修改已被保存';
+$lang['profile_edit_logo_title'] = '上传新的个人资料图片';
+$lang['profile_edit_logo_upload'] = '上传您的个人资料图片';
+$lang['profile_edit_logo_preview'] = '你目前的个人图片';
+$lang['profile_edit_uname'] = '用户名';
+$lang['profile_edit_fname'] = '名字';
+$lang['profile_edit_lname'] = '姓';
+$lang['profile_edit_age'] = '年龄';
+$lang['profile_edit_address'] = '地址';
+$lang['profile_edit_company'] = '公司';
+$lang['profile_edit_phone'] = '电话';
+$lang['profile_edit_email'] = '电子邮件';
+$lang['profile_edit_password'] = '密码';
+$lang['profile_form_upload_error'] = '对不起，无法上传资料图片';
+$lang['profile_form_fail_email'] = '电子邮件已经存在该用户';
+$lang['profile_form_fail'] = '对不起，您的个人资料没有保存';
+$lang['profile_form_success'] = '成功，保存您的个人资料信息';
+$lang['profile_form_submit_button'] = '更新个人信息';
+	
+ 	
+//Delete	//删除
+$lang['profile_delete_profile'] = '个人资料';
+$lang['profile_delete_warning'] = '你要删除';
+$lang['profile_delete_uname'] = '用户名 - ';
+$lang['profile_delete_confirm'] = '确认删除';
+$lang['profile_delete_cancel'] = '删除个人资料';
+ 	
+//Button sub-menu	//按钮子菜单
+//	//
+$lang['submenu_select_groups'] = '选择共享活动';
+$lang['submenu_select_categories'] = '选择分类';
+$lang['submenu_select_sources'] = '选择来源';
+$lang['submenu_dropdown_all_locations'] = '所有位置';
+$lang['submenu_dropdown_all_categories'] = '所有类别';
+$lang['submenu_dropdown_all_sources'] = '所有资源';
+	
+  	
+//Calendar	//日历
+//	//
+$lang['calendar_title'] = '活动';
+$lang['calendar_title_public'] = '公共事件';
+$lang['calendar_loading_title'] = ' 加载请稍候。';
+$lang['calendar_export'] = '出口';
+$lang['calendar_import'] = '进口';
+	
+//modal	//模式
+$lang['calendar_modal_create_title'] = '创建活动';
+$lang['calendar_modal_update_title'] = '更新事件';
+$lang['calendar_modal_tabtitle'] = '信息';
+$lang['calendar_modal_tabtitle2'] = '类别';
+$lang['calendar_modal_tabtitle3'] = '经常性';
+$lang['calendar_modal_tabtitle4'] = '位置';
+$lang['calendar_modal_tabtitle5'] = '连接';
+$lang['calendar_modal_eventname'] = '标题';
+$lang['calendar_modal_description'] = '描述';
+$lang['calendar_modal_eventbegin'] = '开始事件';
+$lang['calendar_modal_eventend'] = '结束事件';
+$lang['calendar_modal_eventurl'] = 'URL链接';
+$lang['calendar_modal_eventallday'] = '整天';
+$lang['calendar_modal_eventshare'] = '分享活动';
+$lang['calendar_modal_eventprivate'] = '私人事件';
+$lang['calendar_modal_eventpublic'] = '公共事件';
+$lang['calendar_modal_eventrecurring'] = '周期性活动';
+$lang['calendar_modal_eventendrecurring'] = '结束日期';
+$lang['calendar_modal_eventnonerecurring'] = '非循环';
+$lang['calendar_modal_eventdaily'] = '日常';
+$lang['calendar_modal_eventweekly'] = '每周';
+$lang['calendar_modal_event2weeks'] = '每2周';
+$lang['calendar_modal_eventmonthly'] = '每月';
+$lang['calendar_modal_eventyearly'] = '每年';
+$lang['calendar_modal_category'] = '类别';
+$lang['calendar_modal_eventrendering'] = '放置';
+$lang['calendar_modal_eventfgrender'] = '前景';
+$lang['calendar_modal_eventbgrender'] = '背景';
+$lang['calendar_modal_eventoverlap'] = '交叠';
+$lang['calendar_modal_eventcategory'] = '类别';
+$lang['calendar_modal_colorbackground'] = '背景颜色';
+$lang['calendar_modal_colortext'] = '文本颜色';
+$lang['calendar_modal_colorborder'] = '边框颜色';
+$lang['calendar_modal_attachment'] = '附加文件';
+$lang['calendar_modal_upload_eventsource'] = '上传活动源';
+$lang['calendar_modal_upload_event_message'] = '从您的计算机中选择iCal文件（ICS）';
+$lang['calendar_modal_upload_save'] = '上传的iCal';
+	
+//Delete	//删除
+$lang['calendar_delete_event'] = '删除事件';
+$lang['calendar_delete_warning'] = '你要删除';
+	
+//Message	//信息
+$lang['calendar_message_title'] = '不活动';
+$lang['calendar_message_warning'] = '没有在队列中可用的事件';
+$lang['calendar_message_success'] = '活动已成功更新';
+$lang['calendar_message_queue'] = '待审批';
+	
+ 	
+//Notify	//通知
+//	//
+$lang['notify_email_public_event'] = '新的公共事件提供';
+$lang['notify_email_delete_user'] = '您的个人资料和内容被删除，请与管理员联系了解更多详情。';
+	
+ 	
+//Gmaps	// GMaps实现。
+//	//
+$lang['locations_all_heading'] = '所有位置';
+	
+//Count	//计数
+$lang['locations_all_confirm_locations'] = '所有确认的地点';
+  	
+  	
+//Categories	//分类
+//	//
+$lang['categories_all_heading'] = '所有类别';
+//Add	//加
+$lang['categories_add_new'] = '添加新类别';
+$lang['categories_input_name'] = '类别名称';
+$lang['categories_input_description'] = '类别说明';
+	
+//Edit	//编辑
+$lang['categories_edit_source'] = '编辑本类别';
+	
+//Delete	//删除
+$lang['categories_delete_category'] = '删除此分类';
+$lang['categories_delete_warning'] = '你要删除';
+	
+//Drag	//拖动
+$lang['categories_draggable_title'] = '可拖动的活动';
+$lang['categories_draggable_removable'] = '下跌后删除事件';
+	
+//Message	//信息
+$lang['categories_message_title'] = '不分类';
+$lang['categories_message_warning'] = '没有可用的类别';
+$lang['categories_message_success'] = '已成功更新';
+	
+ 	
+//Sources	//来源
+//	//
+$lang['sources_all_heading'] = '所有资源';
+//Add	//加
+$lang['sources_add_new'] = '添加新源';
+$lang['sources_input_name'] = '源名称';
+$lang['sources_input_url'] = '源URL';
+	
+//Edit	//编辑
+$lang['sources_edit_source'] = '编辑本源';
+	
+//Delete	//删除
+$lang['sources_del_source'] = '删除此源';
+$lang['sources_delete_warning'] = '你要删除';
+	
+//Message	//信息
+$lang['sources_message_title'] = '没有资源';
+$lang['sources_message_warning'] = '没有可用的源';
+$lang['sources_message_success'] = '已成功更新';
+	
+ 	
+//Settings	//设置
+//	//
+$lang['settings_name'] = '设置';
+$lang['settings_basic_name'] = '基本设置';
+$lang['settings_cal_name'] = '日历设置';
+$lang['settings_file_name'] = '的iCalendar设置';
+$lang['settings_pic_name'] = '图片设置';
+$lang['settings_attach_name'] = '连接设置';
+$lang['settings_template_name'] = '模板设置';
+$lang['settings_theme_name'] = '主题设置';
+	
+	
+//Edit	//编辑
+$lang['settings_form_site_name'] = '网站名称';
+$lang['settings_form_site_email'] = '网站的电子邮件';
+$lang['settings_form_meta_keywords'] = '网站meta关键字';
+$lang['settings_form_meta_description'] = '网站meta描述';
+$lang['settings_form_latitude'] = '纬度';
+$lang['settings_form_longitude'] = '经度';
+$lang['settings_form_timezone'] = '时区';
+$lang['settings_form_captcha'] = '验证码';
+$lang['settings_form_debug'] = '调试';
+$lang['settings_form_submit_button'] = '提交';
+	
+//fullcalendar	// fullcalendar
+$lang['cal_defaultview'] = '默认视图';
+	
+$lang['cal_defaultview_month'] = '月';
+$lang['cal_defaultview_basicweek'] = 'BasicWeek';
+$lang['cal_defaultview_basicday'] = 'BasicDay';
+$lang['cal_defaultview_agendaweek'] = 'AgendaWeek';
+$lang['cal_defaultview_agendaday'] = 'AgendaDay';
+$lang['cal_defaultview_agendalist'] = 'AgendaList';
+$lang['cal_defaultview_timelineDay'] = '时间轴日';
+$lang['cal_defaultview_timelineWeek'] = '时间轴周';
+$lang['cal_defaultview_timelineMonth'] = '时间轴月';
+$lang['cal_defaultview_timelineYear'] = '时间轴年';
+ 	
+$lang['cal_header_left'] = '标题按钮/标题左';
+$lang['cal_header_center'] = '标题按钮/标题中心';
+$lang['cal_header_right'] = '头按钮/所有权的权利';
+	
+$lang['cal_language'] = '语言';
+$lang['cal_timezone'] = '时区';
+$lang['cal_aspectratio'] = '宽高比';
+$lang['cal_hiddendays'] = '周六）';
+$lang['cal_editable'] = '可编辑（对于仅限会员日历）';
+$lang['cal_businesshours'] = '营业时间';
+$lang['cal_businesshours_start'] = '营业时间';
+$lang['cal_businesshours_end'] = '关闭时间';
+$lang['cal_businesshours_opendays'] = '周六）';
+$lang['cal_firstday'] = '第一天';
+$lang['cal_weeknumbers'] = '周数';
+$lang['cal_eventlimit'] = '事件限制';
+$lang['cal_alldayslot'] = 'AllDay插槽';
+$lang['cal_slotduration'] = '时间槽';
+$lang['cal_slotlabeling'] = '老虎标签';
+$lang['cal_slotlabel_groupformat'] = '集团格式（06:00 pm-6:30pm）';
+$lang['cal_slotlabel_listformat'] = '列表格式（下午6:00）';
+$lang['cal_isrtl'] = '查看从右到左';
+ 	
+//Theme 	//主题
+$lang['theme_name'] = '主题';
+	
+//Edit	//编辑
+$lang['profile_picture_title'] = '头像要求';
+$lang['file_title'] = 'ICS文件要求';
+$lang['attach_title'] = '附件文件要求';
+	
+$lang['profile_max_upload_width'] = '宽度';
+$lang['profile_max_upload_height'] = '高度';
+$lang['profile_max_upload_filesize'] = '文件大小（KB）';
+$lang['profile_allowed_extensions'] = '允许扩展';
+	
+$lang['sync_path_location'] = 'ICS文件位置';
+$lang['sync_allowed_extension'] = '允许扩展';
+$lang['sync_max_size'] = '文件大小（KB）';
+	
+$lang['attach_allowed_extension'] = '允许扩展';
+$lang['attach_max_size'] = '文件大小（KB）';
+	
+//Language	//语言
+//	//
+$lang['theme_language'] = '语言';
+//lang choices	//郎选择
+$lang['lang_english'] = '英语';
+$lang['lang_arabic_ma'] = '阿拉伯马';
+$lang['lang_arabic_sa'] = '阿拉伯SA';
+$lang['lang_arabic_tn'] = '阿拉伯TN';
+$lang['lang_arabic'] = '阿拉伯';
+$lang['lang_bulgarian'] = '保加利亚语';
+$lang['lang_catalan'] = '加泰罗尼亚语';
+$lang['lang_czech'] = '捷克';
+$lang['lang_danish'] = '丹麦';
+$lang['lang_german_at'] = '德国AT';
+$lang['lang_german'] = '德语';
+$lang['lang_greek'] = '希腊语';
+$lang['lang_english_au'] = '英语AU';
+$lang['lang_english_ca'] = '英语CA';
+$lang['lang_english_gb'] = '英语GB';
+$lang['lang_english_jm'] = '英语JM';
+$lang['lang_spanish'] = '西班牙';
+$lang['lang_farsi'] = '波斯语';
+$lang['lang_finnish'] = '芬兰';
+$lang['lang_french_ca'] = '法国CA';
+$lang['lang_french'] = '法国';
+$lang['lang_hebrew'] = '希伯来语';
+$lang['lang_hindi'] = '印地语';
+$lang['lang_croatian'] = '克罗地亚';
+$lang['lang_hungarian'] = '匈牙利';
+$lang['lang_indonesian'] = '印度尼西亚';
+$lang['lang_icelandic'] = '冰岛';
+$lang['lang_italian'] = '意大利';
+$lang['lang_japanese'] = '日本';
+$lang['lang_korean'] = '韩国';
+$lang['lang_lithuanian'] = '立陶宛';
+$lang['lang_latvian'] = '拉脱维亚';
+$lang['lang_norwegian'] = '挪威';
+$lang['lang_dutch'] = '荷兰';
+$lang['lang_polish'] = '抛光';
+$lang['lang_portuguese_br'] = '葡萄牙BR';
+$lang['lang_portuguese'] = '葡萄牙语';
+$lang['lang_romanian'] = '罗';
+$lang['lang_russian'] = '俄语';
+$lang['lang_slovak'] = '斯洛伐克';
+$lang['lang_slovenian'] = '斯洛文尼亚';
+$lang['lang_serbian_cyrl'] = '塞尔维亚CYRL';
+$lang['lang_serbian'] = '塞尔维亚';
+$lang['lang_swedish'] = '瑞典';
+$lang['lang_thai'] = '泰国';
+$lang['lang_turkish'] = '土耳其';
+$lang['lang_ukrainian'] = '乌克兰';
+$lang['lang_vietnamese'] = '越南';
+$lang['lang_chinese'] = '中文';
+$lang['lang_chinese_tw'] = '中国TW';
+ 	
+//Error	//错误
+// 	
+$lang['error_not_found'] = '对不起....我们不\ 吨有你正在寻找 的页面';
+$lang['error_page_title'] = '错误';
+$lang['error_not_found_page_title'] = '未找到';
+$lang['error_sending_email'] = '有发送电子邮件的一个问题......与网站管理员联系';
+$lang['error_creating_password'] = '有是创建一个新的密码有问题......与网站管理员联系';
+$lang['error_creating_member'] = '有是创建一个新用户的一个问题......与网站管理员联系';
+$lang['error_captcha'] = '验证码不正确';
+ 
