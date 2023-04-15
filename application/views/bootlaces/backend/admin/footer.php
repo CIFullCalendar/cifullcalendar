6b41e13c961e363cefe2891c45a318bacfa1d9e6
Andre Gardiner <andre@sirdre.com>
2023-04-14 20:56:03 -0600

    Add files via upload

---
+++ application/views/bootlaces/backend/admin/footer.php
@@ -0,0 +1,28 @@
+	<div class="page-footer">
+		<div class="page-footer-inner pull-right">  
+			<p><?php echo lang('current_v') ?><?php echo $current_version ?> - Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+		</div>
+		<div class="scroll-to-top">
+			<i class="fa fa-arrow-circle-o-up"></i>
+		</div>
+	</div>
+	<!-- /.page-footer -->	
+	
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script src="<?php echo base_url();?>assets/plugins/jquery/jquery-3.3.1.min.js" type="text/javascript"></script>
+	<script src="<?php echo base_url();?>assets/plugins/jquery/jquery-ui-1.12.1.min.js" ></script> 
+	<script src="<?php echo base_url();?>assets/plugins/jquery/jquery.slimscroll.min.js" ></script> 
+	<!-- Bootstrap JS -->
+	<script src="<?php echo base_url();?>assets/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript" ></script>  
+	<!-- cifullcalendar JS -->  
+	<script src="<?php echo base_url();?>assets/plugins/markerclusterer/1.0.1/src/markerclusterer_compiled.js" type="text/javascript"></script>
+	<script src="<?php echo base_url();?>assets/plugins/markerclusterer/1.0.1/src/gmap.admin.min.js"></script>
+	<script src="<?php echo base_url();?>assets/plugins/moment/moment.min.js" type="text/javascript" ></script>   
+	<script src="<?php echo base_url();?>assets/plugins/moment/moment-timezone.min.js" type="text/javascript" ></script> 
+	<script src="<?php echo base_url();?>assets/plugins/fullcalendar-3.9.0/fullcalendar.min.js" type="text/javascript"  ></script> 	 
+	<script src="<?php echo base_url();?>assets/plugins/fullcalendar-3.9.0/calendar.admin.custom.min.js" type="text/javascript" ></script>
+	<script src="<?php echo base_url();?>assets/plugins/fullcalendar-3.9.0/locale-all.js" ></script>  
+	<!-- Custom JS -->     
+	<script src="<?php echo base_url();?>assets/bootlaces/js/theme.min.js" type="text/javascript" ></script> 
+	<script src="<?php echo base_url();?>assets/bootlaces/js/layout.min.js" type="text/javascript" ></script>  
+	<script src="<?php echo base_url();?>assets/bootlaces/js/admin.js" type="text/javascript" ></script> 	
---
+++ application/views/bootlaces/backend/home/footer.php
@@ -0,0 +1,30 @@
+	<div class="page-footer">
+		<div class="page-footer-inner pull-right">  
+			<p><?php echo lang('current_v') ?><?php echo $current_version ?> - Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+		</div>
+		<div class="scroll-to-top">
+			<i class="fa fa-arrow-circle-o-up"></i>
+		</div>
+	</div>
+	<!-- /.page-footer -->
+		
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script src="<?php echo base_url();?>assets/plugins/jquery/jquery-3.3.1.min.js" type="text/javascript"></script>  
+	<script src="<?php echo base_url();?>assets/plugins/jquery/jquery-ui-1.12.1.min.js" ></script>  
+	<!-- bootstrap JS -->
+	<script src="<?php echo base_url();?>assets/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript" ></script>   
+    <script src="<?php echo base_url();?>assets/plugins/bootstrap-select/js/bootstrap-select.min.js" ></script>		
+ 	<!-- CIFullcalendar JS --> 
+	<script src="<?php echo base_url();?>assets/plugins/jquery/jquery.ui.touch.min.js" ></script> 
+	<script src="<?php echo base_url();?>assets/plugins/jquery/jquery.minicolors.min.js" ></script>	 
+	<script src="<?php echo base_url();?>assets/plugins/moment/moment.min.js" ></script>  
+	<script src="<?php echo base_url();?>assets/plugins/moment/moment-timezone.min.js" ></script>    
+	<script src="<?php echo base_url();?>assets/plugins/fullcalendar-3.10.0/fullcalendar.js" ></script>
+	<script src="<?php echo base_url();?>assets/plugins/fullcalendar-3.10.0/calendar.custom_offline.js" ></script>
+	<script src="<?php echo base_url();?>assets/plugins/bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js" ></script>
+	<script src="<?php echo base_url();?>assets/plugins/fullcalendar-3.10.0/locale-all.js" ></script>
+	<script src="<?php echo base_url();?>assets/plugins/fullcalendar-3.10.0/gcal.js" ></script>
+	<!-- Custom JS -->	     
+	<script src="<?php echo base_url();?>assets/bootlaces/js/theme.js" type="text/javascript" ></script> 
+	<script src="<?php echo base_url();?>assets/bootlaces/js/layout.js" type="text/javascript" ></script> 
+	<script src="<?php echo base_url();?>assets/bootlaces/js/users.js" type="text/javascript" ></script> 
